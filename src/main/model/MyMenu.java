b5da3b0b8494014609b1502d0fccd10d82433709
steve8D <70412140+steve8D@users.noreply.github.com>
2020-10-11 01:50:23 -0700

    Implemented and added test cases for MyMenu

--- src/main/model/MyMenu.java
+++ src/main/model/MyMenu.java
@@ -1,33 +1,70 @@
 package model;
 
+import model.item.BakedGoods;
+import model.item.Drinks;
 import model.item.Item;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MyMenu {
-    private ArrayList<Item> menu;
+
+
+    private ArrayList<Item> drinks;
+    private ArrayList<Item> bakedGoods;
 
     // EFFECTS: construct a list of items on the menu
     public MyMenu() {
-        menu = new ArrayList<>();
+        drinks = new ArrayList<>();
+        bakedGoods = new ArrayList<>();
     }
 
-    // EFFECTS: return the items on the menu
-    public List<Item> getItems() {
-        return menu;
+    // EFFECTS: return the items in the drink section on the menu
+    public ArrayList<Item> getDrinks() {
+        return drinks;
+    }
+
+    // EFFECTS: return the items in the baked goods section on the menu
+    public ArrayList<Item> getBakedGoods() {
+        return bakedGoods;
+    }
+
+    // REQUIRES: drink item is not on the menu
+    // MODIFIES: this
+    // EFFECTS: add a drink item on the menu
+    public void addDrinks(Drinks i) {
+        if (!drinks.contains(i)) {
+            drinks.add(i);
+        }
+    }
+
+    // REQUIRES: baked goods item is not on the menu
+    // MODIFIES: this
+    // EFFECTS: add a baked goods item on the menu
+    public void addBakedGoods(BakedGoods i) {
+        if (!bakedGoods.contains(i)) {
+            bakedGoods.add(i);
+        }
     }
 
     // MODIFIES: this
-    // EFFECTS: add the item on the menu
-    public void addItem(Item i) {
-        menu.add(i);
+    // EFFECTS: remove the drink item from the menu
+    public void removeDrinks(Drinks i) {
+        drinks.remove(i);
     }
 
-    // REQUIRES: the item must exists on the menu
     // MODIFIES: this
-    // EFFECTS: remove the item from the list
-    public void deleteItem(Item i) {
-        menu.remove(i);
+    // EFFECTS: remove the baked goods item from the menu
+    public void removeBakedGoods(BakedGoods i) {
+        bakedGoods.remove(i);
+    }
+
+    // EFFECTS: returns number of drink items
+    public int getNumberOfDrinks() {
+        return drinks.size();
+    }
+
+    // EFFECTS: returns number of baked good items
+    public int getNumberOfBakedGoods() {
+        return bakedGoods.size();
     }
 }
---
+++ src/test/model/MyMenuTest.java
@@ -0,0 +1,102 @@
+package model;
+
+import model.item.BakedGoods;
+import model.item.Drinks;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MyMenuTest {
+    MyMenu menu;
+    BakedGoods croissant;
+    Drinks brownSugarMilkTea;
+    BakedGoods swissRollCake;
+    Drinks blackCoffee;
+
+    @BeforeEach
+    public void setUp() {
+        menu = new MyMenu();
+        croissant = new BakedGoods("Croissant", 0.5, 20);
+        brownSugarMilkTea = new Drinks("Brown Sugar Milk Tea", 4.5, 50);
+        swissRollCake = new BakedGoods("Swiss Roll Cake", 4, 10);
+        blackCoffee = new Drinks("Iced Black Coffee", 2, 100);
+    }
+
+    @Test
+    public void testConstructor() {
+        assertEquals(0, menu.getNumberOfBakedGoods());
+        assertEquals(0, menu.getNumberOfDrinks());
+    }
+
+    @Test
+    public void testAddDrinks() {
+        menu.addDrinks(brownSugarMilkTea);
+        menu.addDrinks(blackCoffee);
+        assertEquals(2, menu.getNumberOfDrinks());
+    }
+
+    @Test
+    public void testAddDrinksDuplicate() {
+        menu.addDrinks(brownSugarMilkTea);
+        menu.addDrinks(blackCoffee);
+        menu.addDrinks(brownSugarMilkTea);
+        menu.addDrinks(blackCoffee);
+        assertEquals(2, menu.getNumberOfDrinks());
+    }
+
+    @Test
+    public void testAddBakedGoods() {
+        menu.addBakedGoods(croissant);
+        menu.addBakedGoods(swissRollCake);
+        assertEquals(2, menu.getNumberOfBakedGoods());
+    }
+
+    @Test
+    public void testAddBakedGoodsDuplicate() {
+        menu.addBakedGoods(croissant);
+        menu.addBakedGoods(swissRollCake);
+        menu.addBakedGoods(croissant);
+        menu.addBakedGoods(swissRollCake);
+        assertEquals(2, menu.getNumberOfBakedGoods());
+    }
+
+    @Test
+    public void testRemoveDrinks() {
+        // add a drink item and confirms it is there
+        menu.addDrinks(brownSugarMilkTea);
+        assertTrue(menu.getDrinks().contains(brownSugarMilkTea));
+        assertEquals(1, menu.getNumberOfDrinks());
+
+        // remove the drink and confirms it is not on the menu anymore
+        menu.removeDrinks(brownSugarMilkTea);
+        assertFalse(menu.getDrinks().contains(brownSugarMilkTea));
+        assertEquals(0, menu.getNumberOfDrinks());
+    }
+
+    @Test
+    public void testDontRemoveDrinks() {
+        // remove an item that is not added to the menu
+        assertFalse(menu.getDrinks().contains(blackCoffee));
+        menu.removeDrinks(blackCoffee);
+    }
+
+    @Test
+    public void testRemoveBakedGoods() {
+        menu.addBakedGoods(swissRollCake);
+        assertTrue(menu.getBakedGoods().contains(swissRollCake));
+        assertEquals(1, menu.getNumberOfBakedGoods());
+
+        // remove the baked good item and confirms it is not on the menu anymore
+        menu.removeBakedGoods(swissRollCake);
+        assertFalse(menu.getBakedGoods().contains(swissRollCake));
+        assertEquals(0, menu.getNumberOfBakedGoods());
+    }
+
+    @Test
+    public void testDontRemoveBakedGoods() {
+        // remove an item that is not added to the menu
+        assertFalse(menu.getBakedGoods().contains(croissant));
+        menu.removeBakedGoods(croissant);
+    }
+}
